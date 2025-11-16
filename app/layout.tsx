cded5a7c5d729bb466dff1c45f9aa25b1b35bcda
Evan Isnor <ewisnor@gmail.com>
2025-11-16 15:50:56 -0400

    google analytics integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -74,6 +75,7 @@ export default function RootLayout({
         <div className="flex-1">{children}</div>
         <Footer />
       </body>
+      <GoogleAnalytics gaId="G-8NTHNS0E9P" />
     </html>
   );
 }
