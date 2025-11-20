46b0701f61a40fc9a4de2b092a320505beb9c852
Evan Isnor <ewisnor@gmail.com>
2025-11-19 23:56:37 -0400

    disable sentry while in development

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -4,16 +4,19 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-Sentry.init({
-  dsn: "https://8fb67585137d1389c2133519e0db0a01@o4510388698415104.ingest.de.sentry.io/4510388699660368",
+// Only initialize Sentry in production
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: "https://8fb67585137d1389c2133519e0db0a01@o4510388698415104.ingest.de.sentry.io/4510388699660368",
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+    tracesSampleRate: 1,
 
-  // Enable logs to be sent to Sentry
-  enableLogs: true,
+    // Enable logs to be sent to Sentry
+    enableLogs: true,
 
-  // Enable sending user PII (Personally Identifiable Information)
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
-  sendDefaultPii: false,
-});
+    // Enable sending user PII (Personally Identifiable Information)
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+    sendDefaultPii: false,
+  });
+}
